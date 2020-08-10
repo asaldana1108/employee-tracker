444e18050793a94b4381b3dd2474396d39c28697
asaldana1108 <asaldana1108@gmail.com>
2020-08-10 00:28:27 -0500

    inquirer prompts added and query for viewing departments and roles

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# employee-tracker
+# Employee Tracker
+
+## Github URL
+https://github.com/asaldana1108/employee-tracker
+
+## Walkthrough Video
+https://drive.google.com/file/d/10MEwcz2BCkUWTpbPJ0jKyB755Se9NCuq/view
+
+## Description 
+This app creates a simple interface for a content management system. An employee database and created and managed using MYSQL, Node, and Inquirer. 
+
+## Badges
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+npm install
+
+## Usage
+npm start
+
+## License
+MIT
+---
+
+
+
+## Questions
+For any additional questions, here is my contact information. 
+* [GitHub] (github.com/asaldana1108)
+* [Email] (asaldana1108@gmail.com)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 //Create the connection to the database
@@ -9,23 +10,61 @@ const connection = mysql.createConnection({
     // Your MySQL password
     password: 'Atlan!1108',
     database: 'employee_db'
-  });
-  
-  connection.connect(err => {
+});
+
+connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    afterConnection();
-  });
-  
-  afterConnection = () => {
-    // Simple query that will SELECT everything from the 'employee' table
-    // Log the results in the console
-    //
-    connection.query('SELECT * FROM employee', function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-    // YOUR CODE HERE
-    //
-  };
+    // afterConnection();
+    Prompt();
+});
+
+// afterConnection = () => {
+//     // Simple query that will SELECT everything from the 'employee' table
+//     // Log the results in the console
+//     //
+//     connection.query('SELECT * FROM employee', function (err, res) {
+//         if (err) throw err;
+//         console.log(res);
+//         connection.end();
+//     });
+//     // YOUR CODE HERE
+//     //
+// };
+
+function Prompt() {
+    return inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'selection',
+                message: 'What would you like to do?',
+                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+            }
+        )
+        .then(answer => {
+            let option = answer.selection
+            if (option === 'View All Departments') {
+                connection.query(
+                    `SELECT * FROM department`,
+                    function (err, results) {
+                        if (err) throw err;
+                        console.table(results);
+                        Prompt();
+                    }
+                )
+            } else if (option === 'View All Roles') {
+                connection.query(
+                    `SELECT roles.id, title, salary, department_name
+                    FROM roles
+                    LEFT JOIN department
+                    ON roles.department_id = department.id`,
+                    function (err, results) {
+                        if (err) throw err;
+                        console.table(results);
+                        Prompt();
+                    }
+                )
+            }
+        })
+};
